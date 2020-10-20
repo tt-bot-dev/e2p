764bcb0138466095d0537b89784a4123b6b1927e
TTtie <me@tttie.cz>
2020-10-20 22:56:53 +0200

    📝 add copyright preamble to src/tasks.rs
    It was missing :(

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2020 tt.bot dev team
+ * 
+ * This file is part of @tt-bot-dev/e2p.
+ * 
+ * @tt-bot-dev/e2p is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * @tt-bot-dev/e2p is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with @tt-bot-dev/e2p.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 use crate::js_image::*;
 use apng::load_dynamic_image;
 use gif::Encoder as GifEncoder;
