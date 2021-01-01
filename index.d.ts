eb1df2fd26f3335952c665bf546e92b151f88a86
TTtie <me@tttie.cz>
2021-01-01 11:55:49 +0100

    🎉 Happy 2021
    Update licensing year

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2021 tt.bot dev team
+ * 
+ * This file is part of @tt-bot-dev/e2p.
+ * 
+ * @tt-bot-dev/e2p is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * @tt-bot-dev/e2p is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with @tt-bot-dev/e2p.  If not, see <http://www.gnu.org/licenses/>.
+ */
 declare namespace e2p {
     export interface Image {
         data: Buffer;
